288e21c8c888be082ab7eb76ae39aa192d205f69
hieuductran <tranduchieu222004@hotmail.com>
2024-08-10 17:18:14 +0700

    begin 10-08-2024

--- lib/main.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:instagram_clone_ui/app.dart';
-
-void main() {
-  runApp(const MyApp());
-  //TODO : Bài tập : https://youtu.be/M9OPi53xRgE?list=PLNtn1FgzEKBBSHG6noFTyTTSas5oNn5t0&t=920
-  //Xem lại trước khi làm tiếp
-}
